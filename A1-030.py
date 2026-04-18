98da60c9a37b0ab9b88d25c68a919d8839334afd
Ver2Wolf <sanseth.net@gmail.com>
2026-04-18 23:30:55 +0700

    Update A1-030.py

--- A1-030.py
+++ A1-030.py
@@ -1,6 +1,6 @@
 N , pr = int(input()) , list(map(int,input().split()))
-for j in range(2*N-2,-1,-2):
-    pr.pop(j+int(pr[j]>pr[j+1]))
+for j in range(2*N-2,-1,-2) :
+    pr.pop(j + int(pr[j] > pr[j+1]))
 if N<1 : print()
 elif N==1 : print(pr[0])
-else: print(" + ".join(map(str,pr)),"=",sum(pr))
+else : print(" + ".join(map(str,pr)),"=",sum(pr))
