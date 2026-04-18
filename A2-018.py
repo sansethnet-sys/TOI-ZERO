f9d1b4b7af5140270157c41bcc725d5e44cc0d97
Ver2Wolf <sanseth.net@gmail.com>
2026-04-18 23:21:41 +0700

    Update A2-018.py

--- A2-018.py
+++ A2-018.py
@@ -1,5 +1,6 @@
 c , n = input().split()
 cl = ["Red","Green","Blue"]
-while c!=cl[0][0]: cl.append(cl.pop(0))
+while c!=cl[0][0]:
+    cl.append(cl.pop(0))
 for j in range(int(n)):
     print(cl[j%3],end=" ")
