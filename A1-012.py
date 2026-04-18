9b280ecc385d1f3e22ba09210a3a1a1597388d2c
Ver2Wolf <sanseth.net@gmail.com>
2026-04-18 22:55:46 +0700

    Update A1-012.py

--- A1-012.py
+++ A1-012.py
@@ -1,4 +1,4 @@
 nm , pk = int(input()) , input()
 mn = nm%10*10+nm//10
-if pk=="+" : print(nm,pk,mn,"=",nm+mn)
-if pk=="*" : print(nm,pk,mn,"=",nm*mn)
+if pk=="+" : print(nm,"+",mn,"=",nm+mn)
+if pk=="*" : print(nm,"*",mn,"=",nm*mn)
