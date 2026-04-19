a6cd4e0983e91657c7c7c3a9e27b0985061bb757
Ver2Wolf <sanseth.net@gmail.com>
2026-04-19 16:14:43 +0700

    Update A2-026.py

--- A2-026.py
+++ A2-026.py
@@ -1,6 +1,7 @@
 rx , wx , ct = "" , -2**31 , 0
 for i in range(int(input())):
     R , W = input().split()
-    W = int(W) ; ct += (W>15)
+    W = int(W)
+    ct += (W>15)
     if W>wx : rx , wx = R , W
 print(ct,"\n"+rx,wx)
