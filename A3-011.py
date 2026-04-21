2cd7e18331ab0ae98cc1ff197c1dc2fe8e74e122
Ver2Wolf <sanseth.net@gmail.com>
2026-04-21 09:27:35 +0700

    Update A3-011.py

--- A3-011.py
+++ A3-011.py
@@ -1,5 +1,5 @@
 pv , N , pt  = set([]) , int(input()) , list(map(int,input().split()))
 for i in range(N):
     sm = 0
-    for j in pt[i:N]: pv , sm = pv|{sm+j}, sm+j
+    for j in pt[i:N]: pv , sm = pv|{sm+j} , sm+j
 print(len(pv))
