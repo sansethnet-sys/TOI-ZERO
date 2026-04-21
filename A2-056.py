c7d8d9de05afa85c5f3fc5be38e7ca9b2c670759
Ver2Wolf <sanseth.net@gmail.com>
2026-04-21 17:34:47 +0700

    Update A2-056.py

--- A2-056.py
+++ A2-056.py
@@ -1,4 +1,3 @@
-N , tmp = int(input()) , sorted(list(map(int,input().split())))
+N , tmp = int(input()) , sorted(map(int,input().split()))
 for i in tmp:
     if tmp.count(i)==1 : print(i,end=" ")
-if all(tmp.count(i)!=1 for i in tmp): print(-1)
