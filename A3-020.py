b21d5407ca1296878fa3ab78f63349ab38bea4f7
Ver2Wolf <sanseth.net@gmail.com>
2026-04-22 21:45:10 +0700

    Added A3 solution

--- A3-020.py
+++ A3-020.py
@@ -1,4 +1,6 @@
-H1,H2,B1,B2,N,X,Y = map(int,(input()+" 0 "+input()).split())
+H1,H2,B1,B2 = map(int,input().split())
+X,Y = map(int,input().split())
+N = 0
 for I in range(1+min(H1,B1)):
     for J in range(1+min(H2,B2)):
         N = max(N,min(I+J,X)+min(min(H2-J,B1-I)+min(H1-I,B2-J),Y))
