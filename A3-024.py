4773fe2a1ade09baff4df2192a9958cff4781a90
Ver2Wolf <sanseth.net@gmail.com>
2026-04-22 16:13:35 +0700

    Added solution file

--- A3-024.py
+++ A3-024.py
@@ -1,6 +1,7 @@
-cnt , N , L = map(int,[1]+input().split())
+N , L = map(int,input().split())
 vector = [list(map(int, input().split())) for i in range(N)]
+cnt = 1
 for j , k in sorted(vector):
     L = min(L,k)
-    if j>L: cnt , L = cnt+1 , k
+    if j > L: cnt , L = cnt+1 , k
 print(cnt)
