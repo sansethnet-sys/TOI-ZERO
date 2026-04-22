1777ee90a090593b0957289ec95d76499a5d9fc6
Ver2Wolf <sanseth.net@gmail.com>
2026-04-23 00:05:39 +0700

    Updated solution

--- A1-044.py
+++ A1-044.py
@@ -1,7 +1,6 @@
-a , d = input("").split(" ")
-p = int(a)
-if(int(a)<5): p=0
-elif(int(a)<19): p=100
-else: p=150
-if(d=="Wed"): p//=2
+a , d = input().split()
+a , p = int(a) , 150
+if a < 5: p = 0
+elif a < 19: p = 100
+if d == "Wed": p //= 2
 print(p)
