681b3dd5030b49e297fae836f87a1b4f8e64f2db
Ver2Wolf <sanseth.net@gmail.com>
2026-04-22 23:19:05 +0700

    Updated Solution

--- A2-036.py
+++ A2-036.py
@@ -4,4 +4,4 @@ for i in range(n):
     s , t = map(int,input().split())
     tm[s] , tm[t+1] = tm[s]+1 , tm[t+1]-1
 for i in range(1440): tm[i+1] += tm[i]
-print(' '.join([str(tm[int(i)]) for i in input().split()]))
+print(' '.join(map(lambda c:str(tm[int(c)]),input().split())))
