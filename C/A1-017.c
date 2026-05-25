b806c4ada430858cbabeb03eae8f82ee6d3c81d0
Solver <sanseth.net@gmail.com>
2026-05-25 12:34:07 +0700

    A1-solution

--- C/A1-017.c
+++ C/A1-017.c
@@ -2,7 +2,7 @@
 int main(){
     int y1,y2,m2,m1,d1,d2;
     scanf("%d%d%d%d%d%d",&y1,&y2,&m1,&m2,&d2,&d1);
-    d1 += 372*y1+31*m1-31 , d2 += 372*y2+31*m2-31;
+    d1 += 372*y1+31*m1 , d2 += 372*y2+31*m2;
     if(d1>d2) printf("2");
     else if(d1<d2) printf("1");
     else printf("0");
