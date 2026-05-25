e4c147315b726516aaa8ccdc996222589eb22388
Solver <sanseth.net@gmail.com>
2026-05-25 18:22:12 +0700

    A1-solution

---
+++ C++/A1-026.cpp
@@ -0,0 +1,8 @@
+%:include <iostream>
+using namespace std;
+int main()<%
+    cin.tie(0)->sync_with_stdio(0);
+    int nm=3;
+    for(int i=3,j;i--;nm-=j&1) cin>>j;
+    cout<<"even "<<nm<<"\nodd "<<3-nm;
+%>
